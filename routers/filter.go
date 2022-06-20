935e641631573c2422e5e0b0019fb9a32396176e
Yang Luo <hsluoyz@qq.com>
2022-06-20 14:37:59 +0800

    Add repo param.

--- routers/filter.go
+++ routers/filter.go
@@ -1,20 +1,24 @@
 package routers
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 
+	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/context"
 	"github.com/casbin/casbin-website-standalone/util"
 )
 
 func TransparentStatic(ctx *context.Context) {
+	repo := beego.AppConfig.String("repo")
+
 	urlPath := ctx.Request.URL.Path
 	if strings.HasPrefix(urlPath, "/api/") {
 		return
 	}
 
-	path := "../casbin.github.io"
+	path := fmt.Sprintf("../%s", repo)
 	if urlPath == "/" {
 		path += "/zh-CN/index.html"
 	} else {
@@ -32,6 +36,6 @@ func TransparentStatic(ctx *context.Context) {
 	if util.FileExist(path) {
 		http.ServeFile(ctx.ResponseWriter, ctx.Request, path)
 	} else {
-		http.ServeFile(ctx.ResponseWriter, ctx.Request, "../casbin.github.io/zh-CN/index.html")
+		http.ServeFile(ctx.ResponseWriter, ctx.Request, fmt.Sprintf("../%s/zh-CN/index.html", repo))
 	}
 }
