2be8db21fde32cd1eb429de4cd6ba31d819ee671
Yang Luo <hsluoyz@qq.com>
2022-12-24 22:42:45 +0800

    Add headers

--- routers/filter.go
+++ routers/filter.go
@@ -1,3 +1,17 @@
+// Copyright 2022 The casbin Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package routers
 
 import (
@@ -22,10 +36,9 @@ func TransparentStatic(ctx *context.Context) {
 	if urlPath == "/" {
 		http.Redirect(ctx.ResponseWriter, ctx.Request, "/zh", 301)
 		return
-	} else {
-		path += urlPath
 	}
 
+	path += urlPath
 	tokens := strings.Split(path, "/")
 	if len(tokens) > 0 {
 		lastToken := tokens[len(tokens)-1]
